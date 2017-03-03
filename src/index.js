c0289e8a9a5f35558c1a6a37245a578a8c9faab5
nhira <hira@acbc327f6533.ant.amazon.com>
2017-03-02 18:23:35 -0600

    added copyright

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 'use strict';
 
-/**
+/*
+	Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+	Licensed under the Amazon Software License (the "License"). You may not use this file except
+	in compliance with the License. A copy of the License is located at
+		http://aws.amazon.com/asl/
+	or in the "LICENSE.txt" file accompanying this file.
+	This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+	express or implied. See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+/*
  * This code sample demonstrates an implementation of the Lex Code Hook Interface
  * in order to serve a bot which is used to order coffee.
  *
